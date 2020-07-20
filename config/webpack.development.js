3a6153b118490eb79f02f86eda5567bb90b10634
19h47 <jeremylevron@19h47.fr>
2020-07-20 08:12:53 +0200

    :arrow_up: Up

--- config/webpack.development.js
+++ config/webpack.development.js
@@ -4,59 +4,60 @@
  * @author Jérémy Levron <jeremylevron@19h47.fr> (https://19h47.fr)
  */
 
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = merge(
-    common,
-    {
-        output: {
-            filename: 'js/[name].js',
-			chunkFilename: 'js/[name].bundle.js',
-        },
-        mode: 'development',
-        devtool: 'source-map',
-        watch: true,
-        module: {
-            rules: [{
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        publicPath: '../'
-                    }
-                },
-                {
-                    loader: 'css-loader',
-                    options: {
-                        sourceMap: true,
-                    }
-                },
-                {
-                    loader: 'postcss-loader',
-                    options: {
-                        sourceMap: true
-                    },
-                },
-                {
-                    loader: 'sass-loader',
-                    options: {
-						sassOptions: Object.assign({
+module.exports = merge(common, {
+	output: {
+		filename: 'js/[name].js',
+		chunkFilename: 'js/[name].bundle.js',
+	},
+	mode: 'development',
+	devtool: 'source-map',
+	watch: true,
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							publicPath: '../',
+						},
+					},
+					{
+						loader: 'css-loader',
+						options: {
 							sourceMap: true,
-							precision: 10
-						})
-					}
-                }]
-            }],
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: 'css/main.css',
-                publicPath: '../'
-            }),
-        ]
-    },
-);
+						},
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sassOptions: Object.assign({
+								sourceMap: true,
+								precision: 10,
+							}),
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'css/main.css',
+			publicPath: '../',
+		}),
+	],
+});
--- config/webpack.production.js
+++ config/webpack.production.js
@@ -1,13 +1,13 @@
 /**
  *
  * @file   webpack.production.js
- * @author Jérémy Levron <jeremylevron@19h47.fr> (http://19h47.fr)
+ * @author Jérémy Levron <jeremylevron@19h47.fr> (https://19h47.fr)
  */
 
 const glob = require('glob');
 const path = require('path');
 
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
